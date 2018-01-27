812ab0258660ac3ff66344c2669b5519fb4036c0
ENG_OMARSLEAM\omar <ooomar92@gmail.com>
2018-01-27 11:02:25 +0200

    player states

---
+++ Assets/Scripts/RollingPLayerState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingPLayerState : MonoBehaviour ,IPLayerState{
+
+    public void Start(GameObject self)
+    {
+
+    }
+
+
+    public void FixedUpdate(GameObject self)
+    {
+     
+    }
+
+    public void OnEnd(GameObject gameObject)
+    {
+       
+    }
+
+
+
+    public void Update(GameObject self)
+    {
+      
+    }
+}
